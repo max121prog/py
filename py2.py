07662c73462b7cf4ae7d2fa2fed45451d06b118a
max121prog <82708478+max121prog@users.noreply.github.com>
2023-02-19 09:53:19 +0300

    Create py2.py

---
+++ py2.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число для вычисления факториала: \n'))
+while True:
+                    f = 1
+                    while a>=1:
+                                    f = a * f
+                                    a  = a - 1
+                                    print(f)
