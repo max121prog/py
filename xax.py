b0383b14fbfcd820d11cb072621b510bacca975f
max121prog <82708478+max121prog@users.noreply.github.com>
2023-02-19 09:56:49 +0300

    Add files via upload

---
+++ xax.py
@@ -0,0 +1,7 @@
+a = int(input('Input'))
+while True:
+                    f = 1
+                    while a>=1:
+                                    f = a * f
+                                    a  = a - 1
+                                    print(f)
